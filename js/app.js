bf93249e19624f51e9b071f31cb14aa8f7f80bca
honwhy.wang <honwhy.wang@gmail.com>
2018-05-09 14:26:38 +0800

    修改dblclick后没有backup的问题

--- js/app.js
+++ js/app.js
@@ -6,22 +6,24 @@ App.controller('indexCtrl', ['$scope',
             {id: 1, name: 'honey',age: 26, desc: 'like football'},
             {id: 2, name: 'honpp',age: 29, desc: 'like programming'}
         ]
-        var backup = []
+        
+        $scope.backupList = []
         $scope.backup = function($index) {
-            if(!backup[$index]) {
-                backup[$index] = angular.copy($scope.list[$index])
+            if(!$scope.backupList[$index]) {
+                $scope.backupList[$index] = angular.copy($scope.list[$index])
             }
             
         }
         $scope.cancel = function($index) {
             $scope.$broadcast('$eCancel',{index:$index});
-            if(backup[$index]) {
-                $scope.list[$index] = angular.copy(backup[$index])
+            if($scope.backupList[$index]) {
+                $scope.list[$index] = angular.copy($scope.backupList[$index])
             }
-            backup[$index] = null
+            $scope.backupList[$index] = null
         }
         $scope.save = function($index) {
             console.log($scope.list[$index])
+            // and then remove backupList[$index]
         }
     }])
 App.controller("FirstCtrl", function($scope){
