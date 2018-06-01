c810027788e79583d562ab564b3a456623ae7680
honwhy.wang <honwhy.wang@gmail.com>
2018-06-01 11:14:48 +0800

    add position style

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ App.controller('indexCtrl', ['$scope',
     function($scope){
         $scope.list = [
             {id: 1, name: 'honey',age: 26, gender: 'F', mobile: '13700013700', address: 'guangzhou',interest: 'like football'},
-            {id: 2, name: 'honpp',age: 29, gender: 'F', mobile: '18500013700', address: 'shenzhen',interest: 'like programming'}
+            {id: 2, name: 'honpp',age: 29, gender: 'F', mobile: '18500013700', address: 'shenzhen',interest: 'like programming'},
+            {id: 3, name: 'hxxxx',age: 28, gender: 'F', mobile: '11111111111', address: 'suzhou', interest: '脱脂奶粉22%,  浓缩乳清蛋白20%, 植物油19%, 乳糖 18%, 玉米糖浆固体13%, 膳食纤维5.5%, 矿物质 0.9%, 其他 1.6%'}
         ]
         
         $scope.backupList = []
